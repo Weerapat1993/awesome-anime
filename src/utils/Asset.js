794227d3569629b265f48dfb356a8856bd9d3dc8
Weerapat-Dotography <weerapat.t@dotography.com>
2018-01-19 17:13:12 +0700

    Commit Updated

--- src/utils/Asset.js
+++ src/utils/Asset.js
@@ -5,13 +5,13 @@ const Asset = (path, keyword) => {
   const word = keyword.split('(')[0].trim()
   const asset = `./src/assets${path}/${Case.kebab(word)}`
   const noImage = './src/assets/images/no-image.jpg'
-  if (!fs.existsSync(`${asset}.jpg`) && !fs.existsSync(`${asset}.png`)){
-    return noImage
+  if (fs.existsSync(`${asset}.jpg`)){
+    return `${asset}.jpg`
   }
   if (fs.existsSync(`${asset}.png`)){
     return `${asset}.png`
   }
-  return `${asset}.jpg`
+  return noImage
 }
 
 module.exports = Asset
